74efa9bff5fc94e84f23423fb686351d437db7ab
Armando Nuñez_404 <114707950+armandonum@users.noreply.github.com>
2022-12-02 14:33:35 -0400

    Add files via upload

---
+++ arbol_Binario_funcio/[EDD]Tarea3/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace arboles_binarios
+{
+    public partial class Form1 : Form
+    {
+        ABB arbol;
+        public Form1()
+        {
+            InitializeComponent();
+            arbol = new ABB();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            if (String.IsNullOrEmpty(textBox1.Text) == false)
+            {
+                arbol.insertar(textBox1.Text);
+
+                textBox2.Text = arbol.inorden();
+                textBox3.Text = arbol.preorden();
+                textBox4.Text = arbol.postorden();
+            }
+            textBox1.Focus();
+            textBox1.Clear();
+
+           
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            arbol.limpiar();
+
+            textBox2.Text = arbol.inorden();
+            textBox3.Text = arbol.preorden();
+            textBox4.Text = arbol.postorden();
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+     
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ arbol_Binario_funcio/[EDD]Tarea3/NodoABB.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arboles_binarios
+{
+    class NodoABB
+    {
+        public string dato { get; set; }
+        public NodoABB izquierdo { get; set; }
+        public NodoABB derecho { get; set; }
+
+        public NodoABB(string dato)
+        {
+            this.dato = dato;
+            this.izquierdo = null;
+            this.derecho = null;
+        }
+    }
+}
